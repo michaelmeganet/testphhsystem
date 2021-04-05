bc9e8aa90293acf1c3330009546f7cd40ad49f53
Thraxelon <mosint.nagant@gmail.com>
2021-04-05 19:15:53 +0700

    Added comparison check

--- test_dsr/compare_dsr.php
+++ test_dsr/compare_dsr.php
@@ -63,6 +63,7 @@ $periodList = getPeriod();
                 $nextDateMonth = date('Y-m-d', mktime(0, 0, 0, $month + 1, 28, $year));
                 $nextPeriod = substr($nextDate, 2, 2) . substr($nextDateMonth, 5, 2);
                 //GENERATE TABLES
+                echo "===Begin fetch distinct Quono / AID records===<br>";
                 $ordtabPST = "orderlistnew_pst_$period";
                 $nextordtabPST = "orderlistnew_pst_$nextPeriod";
                 $prevordtabPST = "orderlistnew_pst_$prevPeriod";
@@ -94,47 +95,187 @@ $periodList = getPeriod();
                 } else {
                     $qrPDSPST = "SELECT * FROM( " . $qrCurPST . " UNION ALL " . $qrPrevPST . ") AS tem";
                 }
+                echo "<pre style='color:black'>$qrPDSPST</pre>";
                 $objSQLPDSPST = new SQL($qrPDSPST);
                 $legacy_OL_quono_Result = $objSQLPDSPST->getResultRowArray();
                 if (empty($legacy_OL_quono_Result)) {
                     echo "There's no orderlist record for interval of $prevDateTime to $dateTime in $ordtabPST / $nextordtabPST / $prevordtabPST <br>";
+
+                    echo "===End fetch distinct Quono / AID records===<br>";
                 } else {
                     echo "Found " . count($legacy_OL_quono_Result) . " Transactions for interval of $prevDateTime to $dateTime<br>";
+
+                    echo "===End fetch distinct Quono / AID records===<br>";
                     foreach ($legacy_OL_quono_Result as $legacy_OL_quono_datarow) {
                         $lold_quono = $legacy_OL_quono_datarow['quono'];
                         $lold_aid_cus = $legacy_OL_quono_datarow['aid_cus'];
+                        echo "====Begin fetching details for $lold_quono ====<br>";
 
-                        $qrCurPST = "SELECT * "
+                        $qrCurPST2 = "SELECT * "
                                 . "FROM $ordtabPST "
                                 . "WHERE datetimeissue_ol >= '$prevDateTime' "
                                 . "AND datetimeissue_ol <= '$dateTime' "
                                 . "AND `ivdate` <> '0000-00-00' "
                                 . "AND `quono` = '$lold_quono' "
-                                . "AND `aid_cus = $lold_aid_cus";
-                        $qrNextPST = "SELECT * "
+                                . "AND `aid_cus` = $lold_aid_cus";
+                        $qrNextPST2 = "SELECT * "
                                 . "FROM $nextordtabPST "
                                 . "WHERE datetimeissue_ol >= '$prevDateTime' "
                                 . "AND datetimeissue_ol <= '$dateTime' "
-                                . "AND `ivdate` <> '0000-00-00'"
+                                . "AND `ivdate` <> '0000-00-00' "
                                 . "AND `quono` = '$lold_quono' "
-                                . "AND `aid_cus = $lold_aid_cus";
-                        $qrPrevPST = "SELECT * "
+                                . "AND `aid_cus` = $lold_aid_cus";
+                        $qrPrevPST2 = "SELECT * "
                                 . "FROM $prevordtabPST "
                                 . "WHERE datetimeissue_ol >= '$prevDateTime' "
                                 . "AND datetimeissue_ol <= '$dateTime' "
-                                . "AND `ivdate` <> '0000-00-00'"
+                                . "AND `ivdate` <> '0000-00-00' "
                                 . "AND `quono` = '$lold_quono' "
-                                . "AND `aid_cus = $lold_aid_cus";
+                                . "AND `aid_cus` = $lold_aid_cus";
 
                         if (checkTableExists($nextordtabPST) == 'YES') {
-                            $qrPDSPST = "SELECT * FROM( " . $qrNextPST . " UNION ALL " . $qrCurPST . " UNION ALL " . $qrPrevPST . ") AS tem";
+                            $qrPDSPST2 = $qrNextPST2 . " UNION ALL " . $qrCurPST2 . " UNION ALL " . $qrPrevPST2;
                         } else {
-                            $qrPDSPST = "SELECT * FROM( " . $qrCurPST . " UNION ALL " . $qrPrevPST . ") AS tem";
+                            $qrPDSPST2 = $qrCurPST2 . " UNION ALL " . $qrPrevPST2;
                         }
-                        $objSQLPDSPST2 = new SQL($qrPDSPST);
+                        echo "<pre style='color:black'>$qrPDSPST2</pre>";
+                        $objSQLPDSPST2 = new SQL($qrPDSPST2);
                         $legacy_OL_Details = $objSQLPDSPST2->getResultRowArray();
-                        
-                        
+                        $lgc_amountmat = 0;
+                        $lgc_discountmat = 0;
+                        $lgc_gstmat = 0;
+                        $lgc_amountsubtotalmat = 0;
+                        $lgc_amountpmach = 0;
+                        $lgc_discountpmach = 0;
+                        $lgc_gstpmach = 0;
+                        $lgc_amountsubtotalpmach = 0;
+                        $lgc_amountcncmach = 0;
+                        $lgc_discountcncmach = 0;
+                        $lgc_gstcncmach = 0;
+                        $lgc_amountsubtotalcncmach = 0;
+                        $lgc_amountother = 0;
+                        $lgc_discountother = 0;
+                        $lgc_gstother = 0;
+                        $lgc_amountsubtotalother = 0;
+                        $lgc_totalamount = 0;
+                        echo "====End fetching details for $lold_quono ====<br><br>";
+                        echo "==+++ Begin fetching price values +++==<br>";
+                        foreach ($legacy_OL_Details as $datarow) {
+                            $lgc_amountmat += (float) $datarow['amountmat'];
+                            $lgc_discountmat += (float) $datarow['discountmat'];
+                            $lgc_gstmat += (float) $datarow['gstmat'];
+                            $lgc_subtotalmat = (float) $datarow['amountmat'] - (float) $datarow['discountmat'] + (float) $datarow['gstmat'];
+                            $lgc_amountsubtotalmat += $lgc_subtotalmat;
+
+                            $lgc_amountpmach += (float) $datarow['amountpmach'];
+                            $lgc_discountpmach += (float) $datarow['discountpmach'];
+                            $lgc_gstpmach += (float) $datarow['gstpmach'];
+                            $lgc_subtotalpmach = (float) $datarow['amountpmach'] - (float) $datarow['discountpmach'] + (float) $datarow['gstpmach'];
+                            $lgc_amountsubtotalpmach += $lgc_subtotalpmach;
+
+                            $lgc_amountcncmach += (float) $datarow['amountcncmach'];
+                            $lgc_discountcncmach += (float) $datarow['discountcncmach'];
+                            $lgc_gstcncmach += (float) $datarow['gstcncmach'];
+                            $lgc_subtotalcncmach = (float) $datarow['amountcncmach'] - (float) $datarow['discountcncmach'] + (float) $datarow['gstcncmach'];
+                            $lgc_amountsubtotalcncmach += $lgc_subtotalcncmach;
+
+                            $lgc_amountother += (float) $datarow['amountother'];
+                            $lgc_discountother += (float) $datarow['discountother'];
+                            $lgc_gstother += (float) $datarow['gstother'];
+                            $lgc_subtotalother = (float) $datarow['amountother'] - (float) $datarow['discountother'] + (float) $datarow['gstother'];
+                            $lgc_amountsubtotalother += $lgc_subtotalother;
+
+                            $lgc_totalamount += ($lgc_subtotalmat + $lgc_subtotalpmach + $lgc_subtotalcncmach + $lgc_subtotalother);
+                        }
+                        $lgc_set_array = array(
+                            'amountmat' => $lgc_amountmat,
+                            'discountmat' => $lgc_discountmat,
+                            'gstmat' => $lgc_gstmat,
+                            'amountsubtotalmat' => $lgc_amountsubtotalmat,
+                            'amountpmach' => $lgc_amountpmach,
+                            'discountpmach' => $lgc_discountpmach,
+                            'gstpmach' => $lgc_gstpmach,
+                            'amountsubtotalpmach' => $lgc_amountsubtotalpmach,
+                            'amountcncmach' => $lgc_amountcncmach,
+                            'discountcncmach' => $lgc_discountcncmach,
+                            'gstcncmach' => $lgc_gstcncmach,
+                            'amountsubtotalcncmach' => $lgc_amountsubtotalcncmach,
+                            'amountother' => $lgc_amountother,
+                            'discountother' => $lgc_discountother,
+                            'gstother' => $lgc_gstother,
+                            'amountsubtotalother' => $lgc_amountsubtotalother,
+                            'totalamount' => $lgc_totalamount
+                        );
+//                        echo "legacy price set = <br>";
+//                        echo "\$lgc_amountmat = " . $lgc_amountmat . "<br>";
+//                        echo "\$lgc_discountmat = " . $lgc_discountmat . "<br>";
+//                        echo "\$lgc_gstmat = " . $lgc_gstmat . "<br>";
+//                        echo "\$lgc_amountsubtotalmat = " . $lgc_amountsubtotalmat . "<br>";
+//                        echo "\$lgc_amountpmach = " . $lgc_amountpmach . "<br>";
+//                        echo "\$lgc_discountpmach = " . $lgc_discountpmach . "<br>";
+//                        echo "\$lgc_gstpmach = " . $lgc_gstpmach . "<br>";
+//                        echo "\$lgc_amountsubtotalpmach = " . $lgc_amountsubtotalpmach . "<br>";
+//                        echo "\$lgc_amountcncmach = " . $lgc_amountcncmach . "<br>";
+//                        echo "\$lgc_discountcncmach = " . $lgc_discountcncmach . "<br>";
+//                        echo "\$lgc_gstcncmach = " . $lgc_gstcncmach . "<br>";
+//                        echo "\$lgc_amountsubtotalcncmach = " . $lgc_amountsubtotalcncmach . "<br>";
+//                        echo "\$lgc_amountother = " . $lgc_amountother . "<br>";
+//                        echo "\$lgc_discountother = " . $lgc_discountother . "<br>";
+//                        echo "\$lgc_gstother = " . $lgc_gstother . "<br>";
+//                        echo "\$lgc_amountsubtotalother = " . $lgc_amountsubtotalother . "<br>";
+//                        echo "\$lgc_totalamount = " . $lgc_totalamount . "<br>";
+                        echo "==+++ End fetching price values +++==<br>";
+                        echo "==Compare to new function==<br>";
+                        $qrNewFunction = "SELECT * FROM $tab WHERE quono = '$lold_quono' AND aid_cus = $lold_aid_cus AND date = '$date'";
+                        $objSQLNewFunction = new SQL($qrNewFunction);
+                        $NF_dataset = $objSQLNewFunction->getResultOneRowArray();
+                        if (empty($NF_dataset)) {
+                            echo "<b> CANNOT COMPARE, THERE'S NO DATA IN NEW FUNCTION DATASET WITH QUONO = $lold_quono AND aid_cus = $lold_aid_cus in date = $date<br>";
+                        } else {
+                            echo "Comparing : <br>";
+                            echo "==DATE = $date<br>";
+                            echo "==QUONO = $lold_quono<br>";
+                            echo "==SALESPERSON = {$NF_dataset['salesperson']}<br>";
+                            unset($NF_dataset['dsrid']);
+                            unset($NF_dataset['date']);
+                            unset($NF_dataset['quono']);
+                            unset($NF_dataset['aid_cus']);
+                            unset($NF_dataset['salesperson']);
+                            unset($NF_dataset['currency']);
+                            echo "<table class='table table-bordered table-responsive'>";
+                            echo "<thead>";
+                            echo "<tr>";
+                            echo "<th> Table Name </th>";
+                            foreach ($lgc_set_array as $key => $priceval) {
+                                echo "<th>$key</th>";
+                            }
+                            echo "</tr>";
+                            echo "</thead>";
+                            echo "<tbody>";
+                            echo "<tr>";
+                            echo "<td> LEGACY </td>";
+                            foreach ($lgc_set_array as $key => $priceval) {
+                                if (($lgc_set_array[$key]) == ($NF_dataset[$key])) {
+                                    echo "<td class='bg-primary'>".($priceval)."</td>";                                    
+                                } else {
+                                    echo "<td class='bg-danger'>".($priceval)."</td>";
+                                }
+                            }
+                            echo "</tr>";
+                            echo "<tr>";
+                            echo "<td> NEW_FUNCTION </td>";
+                            foreach ($NF_dataset as $key => $priceval) {
+                                if (($lgc_set_array[$key]) == ($NF_dataset[$key])) {
+                                    echo "<td class='bg-primary'>".($priceval)."</td>";                                    
+                                } else {
+                                    echo "<td class='bg-danger'>".($priceval)."</td>";
+                                }
+                            }
+                            echo "</tr>";
+                            echo "</tbody>";
+                            echo "</table>";
+                        }
+                        echo "==End Compare to new function==<br>";
                     }
                 }
             }
